86d6b6f32e24f9288b9454ddd11f21f16e59e8c1
Eric S Penrose <ericspenrose@gmail.com>
2020-09-12 10:37:58 -0500

    Missed another spot

--- Movie_df_cleaner.py
+++ Movie_df_cleaner.py
@@ -1,6 +1,6 @@
 #%%
 import pandas as pd
-from matplotlib import pyplot as pyplot
+from matplotlib import pyplot as plt
 from scipy import stats as st
 import numpy as np
 from config import key as OMDBkey
